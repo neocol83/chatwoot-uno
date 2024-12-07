5a4de01fbeff5b127c3e247f7932edbdaa11fa67
neocol83 <moscorrofio10@hotmail.com>
2024-12-06 23:37:35 -0500

    Change translate i18n Chatzi

--- app/javascript/dashboard/i18n/locale/en/index.js
+++ app/javascript/dashboard/i18n/locale/en/index.js
@@ -1,4 +1,3 @@
-import components from './components.json';
 import advancedFilters from './advancedFilters.json';
 import agentBots from './agentBots.json';
 import agentMgmt from './agentMgmt.json';
@@ -13,13 +12,9 @@ import contact from './contact.json';
 import contactFilters from './contactFilters.json';
 import conversation from './conversation.json';
 import csatMgmt from './csatMgmt.json';
-import customRole from './customRole.json';
-import datePicker from './datePicker.json';
 import emoji from './emoji.json';
-import general from './general.json';
 import generalSettings from './generalSettings.json';
 import helpCenter from './helpCenter.json';
-import inbox from './inbox.json';
 import inboxMgmt from './inboxMgmt.json';
 import integrationApps from './integrationApps.json';
 import integrations from './integrations.json';
@@ -32,13 +27,16 @@ import search from './search.json';
 import setNewPassword from './setNewPassword.json';
 import settings from './settings.json';
 import signup from './signup.json';
-import sla from './sla.json';
 import teamsSettings from './teamsSettings.json';
 import whatsappTemplates from './whatsappTemplates.json';
 import webphone from './webphone.json';
+import sla from './sla.json';
+import inbox from './inbox.json';
+import general from './general.json';
+import datePicker from './datePicker.json';
+import customRole from './customRole.json';
 
 export default {
-  ...components,
   ...advancedFilters,
   ...agentBots,
   ...agentMgmt,
--- app/javascript/dashboard/i18n/locale/es/index.js
+++ app/javascript/dashboard/i18n/locale/es/index.js
@@ -29,6 +29,12 @@ import settings from './settings.json';
 import signup from './signup.json';
 import teamsSettings from './teamsSettings.json';
 import whatsappTemplates from './whatsappTemplates.json';
+import webphone from './webphone.json';
+import sla from './sla.json';
+import inbox from './inbox.json';
+import general from './general.json';
+import datePicker from './datePicker.json';
+import customRole from './customRole.json';
 
 export default {
   ...advancedFilters,
@@ -60,6 +66,12 @@ export default {
   ...setNewPassword,
   ...settings,
   ...signup,
+  ...sla,
   ...teamsSettings,
   ...whatsappTemplates,
+  ...inbox,
+  ...general,
+  ...datePicker,
+  ...customRole,
+  ...webphone,
 };
